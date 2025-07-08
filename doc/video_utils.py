7825611e41cc13ebef57e11ffbbd53d3f70b9fb5
j.kim.ryno <63462803+john123zerg@users.noreply.github.com>
2025-07-08 11:26:03 -0500

    Create video_utils.py

---
+++ doc/video_utils.py
@@ -0,0 +1,36 @@
+import cv2
+import os
+from tqdm import tqdm
+from natsort import natsorted  # 추가: 자연스러운 정렬을 위해 natsort import
+
+def create_video_from_images(image_folder, output_video_path, frame_rate=25):
+    # define valid extension
+    valid_extensions = [".jpg", ".jpeg", ".JPG", ".JPEG", ".png", ".PNG"]
+    
+    # get all image files in the folder
+    image_files = [f for f in os.listdir(image_folder) 
+                   if os.path.splitext(f)[1] in valid_extensions]
+    image_files = natsorted(image_files)  # 자연스러운 정렬 적용
+    print(image_files)
+    if not image_files:
+        raise ValueError("No valid image files found in the specified folder.")
+    
+    # load the first image to get the dimensions of the video
+    first_image_path = os.path.join(image_folder, image_files[0])
+    first_image = cv2.imread(first_image_path)
+    height, width, _ = first_image.shape
+    
+    # create a video writer
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v') # codec for saving the video
+    video_writer = cv2.VideoWriter(output_video_path, fourcc, frame_rate, (width, height))
+    
+    # write each image to the video
+    for image_file in tqdm(image_files):
+        image_path = os.path.join(image_folder, image_file)
+        image = cv2.imread(image_path)
+        video_writer.write(image)
+    
+    # source release
+    video_writer.release()
+    print(f"Video saved at {output_video_path}")
+
